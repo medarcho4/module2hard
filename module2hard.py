f31a133cf4f3713969076d5bf1a091f5e58a1f04
medarcho4 <adammazhidov7@gmail.com>
2024-07-24 20:03:26 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,14 @@
+import random
+n=random.randint(3, 20)
+result = ""
+for i in range(1, n):
+    for j in range(i + 1, n):
+        if n % (i+j) == 0:
+            result += f"{i}{j}"
+print(n)
+print(result)
+
+
+
+
+
